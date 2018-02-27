11bae3bf6d36f55e75200a3eab173a59f2e17f1b
Edmundo Rubio <zerphyr.r@gmail.com>
2018-02-27 06:53:19 -0700

    add holder.js

---
+++ 01-JS-Algo/00-3-holder.js
@@ -0,0 +1,20 @@
+// Map 
+//     - executes the function to each element 
+//     - Return New array.
+//     - Do Not Modify Origina array
+
+
+let arr1 = [1, 2, 3, 4, 5, 6]
+console.log(arr1, " .\n")
+
+let arr2 = arr1.map( (x) => { return x * 2 } )
+console.log(arr1)
+console.log(arr2)
+
+
+// --------------------------------------------------------------------
+
+// Filter 
+//     - executes the function to each element 
+//     - Return New array.
+//     - Do Not Modify Origina array
