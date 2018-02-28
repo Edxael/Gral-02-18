9795c860ac9f21855c768be15992cdda153b4f59
Edmundo Rubio <zerphyr.r@gmail.com>
2018-02-28 13:03:15 -0700

    change the title of the project

--- 02-React/01-mycomps/src/App.jsx
+++ 02-React/01-mycomps/src/App.jsx
@@ -14,7 +14,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello...</h1>
+        <h1>MY FAVORITE SINGERS</h1>
         { this.exe1() }
       </div>
     )
