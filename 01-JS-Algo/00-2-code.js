09d38afc19c184cd5fd3811f7c011324129546eb
Edmundo Rubio <zerphyr.r@gmail.com>
2018-03-04 13:37:02 -0700

    new kata

--- 01-JS-Algo/00-2-code.js
+++ 01-JS-Algo/00-2-code.js
@@ -1,22 +1,10 @@
 console.log("\nHello from the CODE.JS File... \n\n");
 
-// Find the duplicated number in the given array
+// Create a function that receives a string and returns the string reversed. 
 
-const myArr = [1, 2, 3, 4, 7, 5, 6, 8, 2, 9, 10, 11, ]
-console.log(myArr)
+const word = 'Helio Training'
+console.log(word)
 
-
-
-const dup = (arr) => {
-    return arr
-            .map( (c, i, a) => { if(i !== a.indexOf(c)) { return `The duplicated is: ${c}` } } )
-            .filter((f) => { return f !== undefined })
-            .reduce( (r) => { return r } )
-}
-
-
-
-console.log(dup(myArr))
 
 
 
