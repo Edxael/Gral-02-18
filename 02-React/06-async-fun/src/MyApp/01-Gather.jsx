29225ef18512fa34ee1f14f9fa40af913e1222f2
Edmundo Rubio <zerphyr.r@gmail.com>
2018-03-06 22:50:15 -0700

    render titles

--- 02-React/06-async-fun/src/MyApp/01-Gather.jsx
+++ 02-React/06-async-fun/src/MyApp/01-Gather.jsx
@@ -3,24 +3,39 @@ let myURL = "https://swapi.co/api/films"
 
 export default class extends React.Component{
 
+    state = { movies: [], show: false }
 
 
-    exe1 = async () => {
-
+    getData = async () => {
+        console.log("Executing getData()");
+        
         const data1 = await fetch(myURL)
         const data2 = await data1.json()
-        const data3 = data2.results
+        const data3 = await data2.results
+        console.log( "Data-3: ", data3 , " \n.")
 
-        console.log( data3 )
-        
-  
+        this.setState({ movies: data3, show: true })
     }
+
+
+    showData = () => {
+        console.log('Execting ShowData()')
+        console.log( "State.Movs: ", this.state.movies );
+        return this.state.movies.map( (film) => { return <h4 key={film.title} >Movie: { film.title }</h4> } )
+    }
+
 
     render(){
         return(
             <div>
                 <h1>StarWars  --  API  - 2</h1>
-                <button onClick={this.exe1.bind(this)} >Execute API</button>
+                <button onClick={this.getData.bind(this)} >Execute API</button>
+                <br/>
+                <hr/>
+
+                <div>
+                    { this.state.show ? React.createElement(this.showData) : <div>Movies Here..</div> }
+                </div>
             </div>
         )
     }
