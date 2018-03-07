d3c7b32d0bb72b1e675d42638a8875a5891871b4
Edmundo Rubio <zerphyr.r@gmail.com>
2018-03-07 00:51:35 -0700

    clean code

--- 02-React/07-async-js-exe/src/MyApp/01-Main.jsx
+++ 02-React/07-async-js-exe/src/MyApp/01-Main.jsx
@@ -3,6 +3,7 @@ import * as MyProm from './02-Promises'
 
 export default class extends React.Component{
 
+
     runPromise = async () => {
         console.log('Runing Promise')
         
@@ -11,9 +12,9 @@ export default class extends React.Component{
         const data1 = MyProm.conditionallyReject(-1)
 
         console.log( data1 )
-        
     }
 
+    
     render(){
         return(
             <div>
