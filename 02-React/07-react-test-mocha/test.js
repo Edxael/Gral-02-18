6195d752e7b888c124bcad544ec293062444265a
Edmundo Rubio <zerphyr.r@gmail.com>
2018-03-08 09:11:19 -0700

    add new test

--- 02-React/07-react-test-mocha/test.js
+++ 02-React/07-react-test-mocha/test.js
@@ -2,22 +2,10 @@ const Code = require('code')
 const expect = Code.expect
 const MyMath = require('./src/MyApp/Math/calc.js')
 
+it('Exist', () => {
+    expect(MyMath()).to.exist()
+})
+
 it('returns a number ....', () => {
     expect(MyMath(2, 7)).to.be.a.number()
 })
-
-
-// expect(true).to.be.a.boolean().and.to.not.equal(false)
-
-
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
