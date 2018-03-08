f2ff28f16985183a1b76536fa2792f9cfa5548ae
Edmundo Rubio <zerphyr.r@gmail.com>
2018-03-07 18:24:52 -0700

    add methods

--- 02-React/07-react-test-mocha/src/MyApp/00-Gather.jsx
+++ 02-React/07-react-test-mocha/src/MyApp/00-Gather.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import * as MyMath from './Math/01-Calc.js'
 
 export default class extends React.Component{
+
+    Exe1(){
+        console.log("Hello from Exe1");
+        console.log( MyMath.add(2, 6) )
+    }
+
     render(){
         return(
             <div>
                 <h1>Hello from gather</h1>
+                <br/>
+
+                <button onClick={this.Exe1.bind(this)} >Execute</button>
             </div>
         )
     }
