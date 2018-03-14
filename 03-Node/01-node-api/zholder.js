1ad06c82cd2ca15d833f4e5e5062e08f3022fd46
Edmundo Rubio <zerphyr.r@gmail.com>
2018-03-14 16:18:30 -0600

    add the file-tree

--- 03-Node/01-node-api/zholder.js
+++ 03-Node/01-node-api/zholder.js
@@ -280,6 +280,22 @@ server.listen(5000, () => {
 // ===========================================================================================
 // CRUD COMPLETE BEFORE GOIN MODULAR
 // ===========================================================================================
+
+  ---FILE-TREE---
+
+[root]
+    |--index.js
+    |--package.json
+    |
+    |--[config]
+    |       |--serverFunctions.js
+    |
+    |--[controllers]
+            |--apiCalls.js
+            |--setup-controllers.js
+
+            
+
 // ------- FUNCTIONS FOR DRY CODE ----------
 const respond = ( res, next, status, data, http_code ) => {
     let response = {
@@ -422,5 +438,151 @@ server.listen(5000, () => {
 //                             CRUD COMPLETE MODULAR CONFIGURATION
 // ===========================================================================================
 
-// FILE [ index.js ]
-// -----------------
+
+// -------------------------------------------------------------------------------
+// FILE [ index.js ('root') ]
+        // ------- IMPORTING -----------
+        const restify = require('restify')
+        const server = restify.createServer()
+        const setupController = require('./controllers/setup-controllers.js')
+        const userController = require('./controllers/apiCalls.js')
+
+
+        // ------- CONTROLLERS -----------
+        setupController(server, restify)
+        userController(server)
+
+
+        // ------- SERVER LISTENER -----------
+        server.listen(5000, () => {
+        console.log('%s listening at %s', server.name, server.url)
+        })
+// -------------------------------------------------------------------------------
+
+
+
+// -------------------------------------------------------------------------------
+// FILE [ serverFunctions.js ('root/config/serverFunctions.js') ]
+        _respond = ( res, next, status, data, http_code ) => {
+            let response = {
+                'status': status,
+                'data': data
+            }
+            res.setHeader('content-type', 'aplication/json')
+            res.writeHead(http_code)
+            res.end(JSON.stringify(response))
+            return next()
+        }
+
+        module.exports.success = (res, next, data) => {
+            _respond(res, next, 'success', data, 200)
+        }
+
+        module.exports.failure = (res, next, data, http_code) => {
+            _respond(res, next, 'failure', data, http_code)
+        }
+// -------------------------------------------------------------------------------
+
+
+
+
+
+// -------------------------------------------------------------------------------
+// FILE [ apiCalls.js ('root/controllers/apiCalls.js') ]
+        const myFuns = require('../config/serverFunctions')
+
+        // ------- FAKE DB ---------
+        let users = {
+            '1': 
+        { first_name: 'Ayumi',
+            last_name: 'Hamasaki',
+            email_address: 'ayu@avex.com.jp',
+            id: 1 },
+            "2": {
+                first_name: "Haruka",
+                last_name: "Kodoma",
+                email_address: "harupy@htk48.com",
+                id: 2
+            }
+        }
+
+        let max_user_id = 2
+        // --------------------------------
+            
+            // *****************************************
+            // ************* API CALLS *****************
+
+        module.exports = (server) => {
+
+
+            // Requesting all users fro DB
+            server.get("/", (req, res, next) => {
+                myFuns.success(res, next, users)
+            })
+
+            // Requesting a user by ID
+            server.get("/user/:id", (req, res, next) => {
+                if(typeof users[req.params.id] === 'undefined'){
+                    myFuns.failure(res, next, 'The specify user not exist in DB', 404)
+                }else{
+                    myFuns.success(res, next, users[parseInt(req.params.id)])
+                }
+                
+            })
+
+            // Add User to DB
+            server.post("/user", (req, res, next) => {
+                let user = req.body
+                
+                max_user_id++
+                user.id = max_user_id
+                users[user.id] = user
+                
+                myFuns.success(res, next, user)
+
+            });
+
+            // Update User
+            server.put("/user/:id", (req, res, next) => {
+                if(typeof users[req.params.id] === 'undefined'){
+                    myFuns.failure(res, next, 'The specify user not exist in DB', 404)
+                }else{
+                    let user = users[ req.params.id ]
+                    let updates = req.body
+
+                    for( z in updates ){
+                        user[z] = updates[z]
+                    }
+
+                    myFuns.success(res, next, user)
+                }
+
+            })
+
+            // Delete a user by ID
+            server.del("/user/:id", (req, res, next) => {
+                if(typeof users[req.params.id] === 'undefined'){
+                    myFuns.failure(res, next, 'The specify user not exist in DB', 404)
+                }else{
+                    delete users[ req.params.id ]
+                    myFuns.success(res, next, [])
+                }
+                
+            })
+
+
+        }
+// -------------------------------------------------------------------------------
+
+
+
+
+// -------------------------------------------------------------------------------
+// FILE [ setup-controllers.js ('root/controllers/setup-controllers.js') ]
+        module.exports = (server, restify) => {
+            server.use(restify.plugins.acceptParser(server.acceptable))
+            server.use(restify.plugins.queryParser())
+            server.use(restify.plugins.bodyParser())
+        }
+// -------------------------------------------------------------------------------
+
