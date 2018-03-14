777bd7b1be9ff19a97abaf209213effb86b1a3b7
Edmundo Rubio <zerphyr.r@gmail.com>
2018-03-13 23:02:53 -0600

    update server

--- 03-Node/01-node-api/index.js
+++ 03-Node/01-node-api/index.js
@@ -4,6 +4,13 @@ const server = restify.createServer()
 let users = {}
 let max_user_id = 0
 
+server.get("/", (req, res, next) => {
+    res.setHeader('content-type', 'aplication/json')
+    res.writeHead(200)
+    res.end(JSON.stringify(users))
+    return next()
+})
+
 server.listen(5000, () => {
   console.log('%s listening at %s', server.name, server.url)
 })
