b03bdedd483aa6ff95f3fd021a1961f6b74f7e22
Edmundo Rubio <zerphyr.r@gmail.com>
2018-03-15 12:44:08 -0600

    add users route

--- 03-Node/03-Express-API/routes/users.js
+++ 03-Node/03-Express-API/routes/users.js
@@ -1,2 +1,23 @@
 const router = require('express').Router()
 const bcrypt = require('bcryptjs')
+const User = require('../models/1-user') 
+
+router.post('/', (req, res) => {
+    let user = new User({
+        username: req.body.user.username,
+        email: req.body.user.email,
+        passhash: bcrypt.hashSync(req.body.user.pwd, 10)
+    })
+
+    user.save().then(
+        (newuser)  => { 
+            res.json({
+                user: newuser,
+                message: 'Success in creating user..'
+            })
+        },
+        (err) => { res.send(500, err.message) }
+    )
+})
+
+module.exports = router
--- 03-Node/03-Express-API/server.js
+++ 03-Node/03-Express-API/server.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.json())
 
 
 app.use('/test', (req, res) => {
     res.send('Hello Edmundo')
 })
 
+app.use('/api/users', require('./routes/users'))
 
 app.listen(5000, (err) => {
     if(err){ throw err }
