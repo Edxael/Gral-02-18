20cd2243cb1a2a09191cd7c7e6d33223b8681dfd
Edmundo Rubio <zerphyr.r@gmail.com>
2018-03-16 15:07:07 -0600

    add zinfo.md file

--- 02-React/17-xml-1/src/MyApp/00-Main.jsx
+++ 02-React/17-xml-1/src/MyApp/00-Main.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 // const MyURL = 'https://api.intacct.com/ia/xml/xmlgw.phtml'
 // const MyURL = 'http://www.xmlsoccer.com/FootballDataDemo.asmx!/CheckApiKey'
+const MyURL = "http://demo9622188.mockable.io/test"
 
 export default class extends Component {
     render(){
@@ -8,61 +9,26 @@ export default class extends Component {
         const CallAPI = () => {
             console.log("Calling the API...")
 
-            var data = JSON.stringify({
-                "foo": "bar"
-              });
-              
-              var xhr = new XMLHttpRequest();
-              xhr.withCredentials = true;
-              
-              xhr.addEventListener("readystatechange", function () {
-                if (this.readyState === this.DONE) {
-                  console.log(this.responseText);
-                }
-              });
-              
-              xhr.open("POST", "http://mockbin.com/request?foo=bar&foo=baz");
-              xhr.setRequestHeader("cookie", "foo=bar; bar=baz");
-              xhr.setRequestHeader("accept", "application/json");
-              xhr.setRequestHeader("content-type", "application/json");
-              xhr.setRequestHeader("x-pretty-print", "2");
-              
-              xhr.send(data);
+            const xhttp = new XMLHttpRequest()
 
+            xhttp.onreadystatechange = function() {
+                if (this.readyState === 4 && this.status === 200) {
+                    console.log(this.responseText)
+                }
+            }
+
+            xhttp.open("GET", MyURL, true)
+            xhttp.send();
             
         }
 
         return(
             <div>
                 <h1>XML - TESTING</h1>
-                <button onClick={CallAPI}>Call XML</button>
+                <button onClick={CallAPI}>Call XML API</button>
             </div>
         )
     }
 }
 
 
-
-
-// http://localhost:5000/api/singers/
-
-// https://<your-host-or-IP>:8443/enterprise/control/agent.php
-
-
-
-// const CallAPI = () => {
-//     console.log("Calling the API...")
-
-//     const XMLCall = new XMLHttpRequest()
-//     XMLCall.onreadystatechange = () => {
-//         if(XMLCall.readyState === XMLHttpRequest.DONE){
-//             console.log("The XML response: ", XMLCall.responseText)
-//         }else{
-//             console.log("It was nt Ready")
-//         }
-//     }
-
-//     XMLCall.open('GET', MyURL, true)
-
-//     XMLCall.send(null)    
-// }
---
+++ 02-React/17-xml-1/zinfo.md
@@ -0,0 +1,21 @@
+# DATA AND INFO 
+
+I might be able to mimic the XML api using the following API-MOCKIN service:
+https://www.mockable.io/a/#/
+
+
+### Location of pages with current data:
+https://www.w3schools.com/xml/ajax_xmlhttprequest_send.asp
+https://www.w3schools.com/xml/ajax_xmlhttprequest_create.asp
+https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
+https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/XMLHttpRequest
+https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+
+
+
+### Axios
+https://www.npmjs.com/package/axios
+
+
+### XML-CALLS WITH POSTMAN
+https://developer.intacct.com/web-services/your-first-api-calls/
