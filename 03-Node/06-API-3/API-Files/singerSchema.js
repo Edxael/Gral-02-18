69c49f551271b82da5256f0f7a1b17065e12250a
Edmundo Rubio <zerphyr.r@gmail.com>
2018-03-15 20:29:52 -0600

    fisnis CRUD calls from React.js to API

---
+++ 03-Node/06-API-3/API-Files/singerSchema.js
@@ -0,0 +1,9 @@
+var mongoose     = require('mongoose')
+var Schema       = mongoose.Schema
+
+var SingerSchema   = new Schema({
+    name: String
+    // email: String,
+});
+
+module.exports = mongoose.model('Bear', SingerSchema)
