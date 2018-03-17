7ca189a04fe24f6be094e9f3c2d1577ddd694ee7
Edmundo Rubio <zerphyr.r@gmail.com>
2018-03-17 16:11:33 -0600

    add new React.js Project

---
+++ 02-React/18-basic-store/src/MyApp/00-Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import db1 from './db/db.js'
+
+export default class extends React.Component{
+    render(){
+        // console.log("als", db1)
+        return(
+            <div>
+                <h1>Hello from Main </h1>
+            </div>
+        )
+    }
+}
---
+++ 02-React/18-basic-store/src/MyApp/db/db.js
@@ -0,0 +1,13 @@
+
+
+
+[
+    {"id:1" , "price": "15" , "name": "Edge or Tomorrow", "year":"2014"  , "rate": "90%", "mpic": "https://github.com/Edxael/Images/blob/master/01-movies/edge-of-tomorrow.jpg?raw=true" },
+    {"id:2" , "price": "12" , "name": "Gattaca", "year": "1997" , "rate": "95%", "mpic": "https://github.com/Edxael/Images/blob/master/01-movies/gattaca.jpg?raw=true" },
+    {"id:3" , "price": "10" , "name": "Salt", "year": "2010"  , "rate": "85", "mpic": "https://github.com/Edxael/Images/blob/master/01-movies/salt-2010.jpg?raw=true" },
+    {"id:4" , "price": "25" , "name": "Underworl: B.W.", "year": "2016"  , "rate": "80%", "mpic": "https://github.com/Edxael/Images/blob/master/01-movies/underworld-blood-wars.jpg?raw=true" },
+    {"id:5" , "price": "11" , "name": "Gladiator", "year": "2000"  , "rate": "99%", "mpic": "https://github.com/Edxael/Images/blob/master/01-movies/gladiator-10th-anniversary-extended-remastered-edition.jpg?raw=true" },
+    {"id:6" , "price": "10" , "name": "The Mummy", "year": "2017"  , "rate": "?", "mpic": "https://github.com/Edxael/Images/blob/master/01-movies/the-mummy.jpg?raw=true" },
+    {"id:6" , "price": "50" , "name": "Enders Game", "year": "2013" , "rate": "100%", "mpic": "https://github.com/Edxael/Images/blob/master/01-movies/enders-game.jpg?raw=true" },
+    {"id:6" , "price": "25" , "name": "Whiplash", "year": "2014" , "rate": "99%", "mpic": "https://github.com/Edxael/Images/blob/master/01-movies/whiplash.jpg?raw=true" }
+  ]
