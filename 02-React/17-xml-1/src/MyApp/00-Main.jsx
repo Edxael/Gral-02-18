f3456d0acd7eb014c1d2558e902f5ad0ffd067f5
Edmundo Rubio <zerphyr.r@gmail.com>
2018-03-17 21:39:01 -0600

    change xmlhttp get request

--- 02-React/17-xml-1/src/MyApp/00-Main.jsx
+++ 02-React/17-xml-1/src/MyApp/00-Main.jsx
@@ -1,13 +1,54 @@
 import React, { Component } from 'react'
+import './styles/00-main.css'
 // const MyURL = "http://demo9622188.mockable.io/test"
+let MyURL = "http://localhost/ajax/01-countries-data.php"
 
 export default class extends Component {
+
+    state = { scon: '' }
+
     render(){
 
         const CallAPI = () => {
             console.log("Calling the API... 07")
+        }
+
+        const getCountries = (e) => {
+            console.log("Runing getCountries")
+
+            this.setState({ scon: this.menu.value })
+
+            setTimeout(() => { 
+                console.log(this.state.scon) 
+
+                let xhr = new XMLHttpRequest()
+
+                MyURL = MyURL + "?continent=" + this.state.scon
+                console.log( MyURL )
+
+                xhr.open("GET", MyURL , true)
+                xhr.send()
+
+                
+                xhr.onreadystatechange = function() {
+                    if(this.readyState === 4 && this.status === 200){
+                        console.log("Paso la prueva");
+                        
+                        // console.log( xhr.responseText )
+                    }
+                }
+
+                
+            
+                MyURL = "http://localhost/ajax/01-countries-data.php"
+                this.setState({ scon: '' })
+            }, 100)
 
             
+
+            
+
+
         }
 
         return(
@@ -16,9 +57,19 @@ export default class extends Component {
                 <hr/>
                 <button onClick={CallAPI}>Call XML API</button>
 
-                <div onChange={CallAPI}>
-                    <input type="text"/>
+                <div className='countries1' >
+
+                    <select id="continents" onChange={getCountries} ref = {(input)=> this.menu = input} >
+                        <option value="africa">Africa</option>
+                        <option value="asia">Asia</option>
+                        <option value="europe">Europe</option>
+                        <option value="n-america">North America</option>
+                        <option value="s-america">South America</option>
+                        <option value="australia">Australia</option>
+                    </select>
+
                 </div>
+
             </div>
         )
     }
