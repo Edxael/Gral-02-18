e40ffd4701790773332d01d73dab82536d8950a1
Edmundo Rubio <zerphyr.r@gmail.com>
2018-03-18 23:19:02 -0600

    adding the flag to ignore the directory node_modules

--- 03-Node/08-Gulp-1/gulp.js
+++ 03-Node/08-Gulp-1/gulp.js
@@ -1,10 +1,4 @@
 const gulp = require('gulp')
 
-/*
-   --- TOP LEVEL FUNCTIONS ---
 
-   gulp.task    --> Define task.
-   gulp.src     --> Point to files to use.
-   gulp.dest    --> Points fo folder output.
-   gulp.watch   --> watch fils and folders for changes. 
-*/
+// Basic task Logs message. 
