b0acfc80420bd20c8b9dd962f487c358deb4d39c
Edmundo Rubio <zerphyr.r@gmail.com>
2018-03-18 23:16:13 -0600

    typeScript

---
+++ 03-Node/08-Gulp-1/gulp.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp')
+
+/*
+   --- TOP LEVEL FUNCTIONS ---
+
+   gulp.task    --> Define task.
+   gulp.src     --> Point to files to use.
+   gulp.dest    --> Points fo folder output.
+   gulp.watch   --> watch fils and folders for changes. 
+*/
